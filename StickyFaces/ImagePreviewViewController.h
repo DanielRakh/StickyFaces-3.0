c9cd78deacbb0ccc32355f4549246667ee1c7013
Daniel Rakhamimov <o0o0noitzdanny@aim.com>
2013-06-25 01:28:31 -0400

    ImagePreview Added...

---
+++ StickyFaces/ImagePreviewViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagePreviewViewController.h
+//  StickyFaces
+//
+//  Created by Daniel Rakhamimov on 6/25/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagePreviewViewController : UIViewController
+
+@end
---
+++ StickyFaces/ImagePreviewViewController.m
@@ -0,0 +1,38 @@
+//
+//  ImagePreviewViewController.m
+//  StickyFaces
+//
+//  Created by Daniel Rakhamimov on 6/25/13.
+//
+//
+
+#import "ImagePreviewViewController.h"
+
+@interface ImagePreviewViewController ()
+
+@end
+
+@implementation ImagePreviewViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
