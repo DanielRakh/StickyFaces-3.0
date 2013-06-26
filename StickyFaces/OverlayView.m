222bb4af6f7b5722c66f8669e3a8eb2cc6d250ef
Daniel Rakhamimov <o0o0noitzdanny@aim.com>
2013-06-26 19:45:09 -0400

    Added transparent view to Camera

--- StickyFaces/OverlayView.m
+++ StickyFaces/OverlayView.m
@@ -8,7 +8,14 @@
 
 #import "OverlayView.h"
 #import "UIBezierPath-Points.h"
+#import <QuartzCore/QuartzCore.h>
 
+@interface OverlayView ()
+
+@property (retain) UIColor *fillColor;
+@property (retain) UIBezierPath *punchedOutPath;
+
+@end
 
 @implementation OverlayView
 
@@ -23,17 +30,74 @@
         
         NSLog(@"OverlayView Frame: %@",NSStringFromCGRect(self.frame));
         NSLog(@"OverlayView Bounds:%@",NSStringFromCGRect(self.bounds));
+        
+        
+        UIBezierPath *windowRect = [UIBezierPath bezierPathWithRect:self.bounds];
+    
+        CAShapeLayer *tintedBackground = [CAShapeLayer layer];
+        tintedBackground.path = windowRect.CGPath;
+        tintedBackground.fillColor = [UIColor colorWithWhite:0.000 alpha:0.760].CGColor;
+    
+//        [self.layer addSublayer:tintedBackground];
+        
+        UIBezierPath *facePath = [self drawFacePath];
+        
+        
+        
+        CAShapeLayer *theOuterRect = [CAShapeLayer layer];
+        theOuterRect.path = facePath.CGPath;
+        //        theOuterRect.bounds = CGRectMake(0,0, radius, radius);
+        //        theDonut.position = CGPointMake(self.bounds.size.width/2.0f, self.bounds.size.height/2.0f);
+        //        theDonut.cornerRadius = radius/2;
+        theOuterRect.fillColor = [UIColor blackColor].CGColor;
+        theOuterRect.fillMode = kCAFillModeBackwards;
+//        theOuterRect.lineWidth  = 320.0f;
+//        theOuterRect.strokeColor = [UIColor colorWithWhite:0.000 alpha:0.500].CGColor;
+        
+//        [self.layer addSublayer:theOuterRect];
+        
+        
+        
+        
+        
+//        static float radius = 150;
+        
+        
+        UIBezierPath *path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, self.bounds.size.width, self.bounds.size.height) cornerRadius:0];
+        
+        UIBezierPath *circlePath = [self drawFacePath];
+        [path appendPath:circlePath];
+        [path setUsesEvenOddFillRule:YES];
+        
+        CAShapeLayer *fillLayer = [CAShapeLayer layer];
+        fillLayer.path = path.CGPath;
+        fillLayer.fillRule = kCAFillRuleEvenOdd;
+        fillLayer.fillColor = [UIColor colorWithWhite:0.000 alpha:0.600].CGColor;
+        fillLayer.opacity = 0.5;
+        [self.layer addSublayer:fillLayer];
+        
+        
+        
+        CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+        shapeLayer.path = facePath.CGPath;
+        shapeLayer.strokeColor = [UIColor colorWithWhite:0.902 alpha:1.000].CGColor;
+        shapeLayer.fillColor = [UIColor clearColor].CGColor;
+        shapeLayer.shadowColor = [UIColor blackColor].CGColor;
+        shapeLayer.shadowOpacity = 0.5;
+        shapeLayer.shadowRadius = 4.0;
+        shapeLayer.lineWidth = 3.0;
+        
+        
+        
+        [self.layer addSublayer:shapeLayer];
+        
     }
     return self;
 }
 
 
 
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
- 
+-(UIBezierPath *)drawFacePath {
     
     CGPoint startingPoint = CGPointMake(0, 58);
     
@@ -118,16 +182,49 @@
     [aPath applyTransform:CGAffineTransformMakeTranslation(0, 30)];
     
     
-
     
-        
-    [aPath stroke];
-    
-
+    aPath.lineWidth = 2.0f;
     
     
-    
+    return aPath;
 }
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+
+
+//
+//- (void)drawRect:(CGRect)rect
+//{
+////    self.fillColor = [UIColor blackColor];
+//////    [[self fillColor] set];
+////    
+////    UIRectFill(rect);
+////    
+////    CGContextRef ctx = UIGraphicsGetCurrentContext();
+////    CGContextSetBlendMode(ctx, kCGBlendModeDestinationOut);
+////    
+////    self.punchedOutPath = [self drawFacePath];
+////    
+////    
+////    [self.punchedOutPath stroke];
+////    
+////    
+////    [[self punchedOutPath] fill];
+////    
+////    CGContextSetBlendMode(ctx, kCGBlendModeNormal);
+////   
+////    self.alpha = 0.5;
+//    
+//    UIBezierPath *facePath = [self drawFacePath];
+//    UIBezierPath *outerPath = [facePath bezierPathByReversingPath];
+//    [[UIColor orangeColor]setFill];
+//    [facePath fill];
+//    [facePath stroke];
+//    
+//}
+
 
 
 @end
